647e2124277bc56effdd1af4dc7ab19ed653e094
Jason <jasonzampas@gmail.com>
2023-10-18 19:56:23 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import random from "random"
+import express from "express"
+const app = express()
+const port = process.env.PORT || 8080
+
+app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: true }))
+
+app.get("/", (req, res) => {
+    res.render("index", {randomNum: null})
+})
+
+app.post("/", (req, res) => {
+    let fromNum = parseInt(req.body.from, 10)
+    let toNum = parseInt(req.body.to, 10)
+
+    if (Number.isInteger(fromNum) && Number.isInteger(toNum)) {
+        let randomNum = random.int(fromNum, toNum)
+
+        res.render("index", { randomNum: randomNum })
+    } else {
+        res.status(400).send("Invalid input values. Please provide valid integers for 'from' and 'to'.")
+    }
+})
+
+app.listen(port, () => { console.log(`Server listening on http://localhost:${port}`)})
