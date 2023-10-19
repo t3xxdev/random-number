5756220a33f646fea515955912534427c7124643
Jason <jasonzampas@gmail.com>
2023-10-19 06:36:34 +0100

    changed some stuff

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const app = express()
 const port = process.env.PORT || 8080
 
 app.set("view engine", "ejs")
+
+app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({ extended: true }))
 
 app.get("/", (req, res) => {
